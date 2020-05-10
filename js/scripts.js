37e0ce8f08b983736fae52759a190aac7b2e860e
Muia23 <wilfred.muema@gmail.com>
2020-05-10 14:04:38 +0300

    Added update to the code

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,18 @@
 $(document).ready(function() {
     $("#blanks form").submit(function() {
-        var person1Input = $(".person1").val();
-        var person2Input = $(".person2").val();
-        var animalInput = $(".animal").val();
-        var exclamationInput = $(".exclamation").val();
-        var verbInput = $(".verb").val();
-        var nounInput = $(".noun").val();
+        var person1Input = $("input#person1").val();
+        var person2Input = $("input#person2").val();
+        var animalInput = $("input#animal").val();
+        var exclamationInput = $("input#exclamation").val();
+        var verbInput = $("input#verb").val();
+        var nounInput = $("input#noun").val();
 
-        $(".person1").append("person1Input");
-        $(".person2").append("person2Input");
-        $(".animal").append("animalInput");
-        $(".exclamation").append("exclamationInput");
-        $(".verb").append("verbInput");
-        $(".noun").append("nounInput");
+        $(".person1").append(person1Input);
+        $(".person2").append(person2Input);
+        $(".animal").append(animalInput);
+        $(".exclamation").append(exclamationInput);
+        $(".verb").append(verbInput);
+        $(".noun").append(nounInput);
 
         $("#story").show();
 
